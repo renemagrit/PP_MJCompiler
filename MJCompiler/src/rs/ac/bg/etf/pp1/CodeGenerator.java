ed0b116eb5f1e7ebd1fac6d2385978ba910eed7a
Marta <martaavramovic015@gmail.com>
2021-06-27 18:09:50 +0200

    Constants and Print CG

--- MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -1,6 +1,9 @@
 package rs.ac.bg.etf.pp1;
 
-import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
+import rs.ac.bg.etf.pp1.ast.*;
+import rs.etf.pp1.mj.runtime.Code;
+import rs.etf.pp1.symboltable.Tab;
+import rs.etf.pp1.symboltable.concepts.*;
 
 public class CodeGenerator extends VisitorAdaptor {
 	private int mainPc;
@@ -8,4 +11,52 @@ public class CodeGenerator extends VisitorAdaptor {
 	public int getMainPc() {
 		return mainPc;
 	}
+	
+	public void visit(FactorNumConst numConst) {
+	   int constValue = ((NumberConstant)numConst.getNumConst()).getN1();
+   	   Code.loadConst(constValue); 
+	}	
+	
+	public void visit(FactorCharConst charConst) {
+	   int constValue = ((CharConstant) charConst.getCharConst()).getC1();
+   	   Code.loadConst(constValue); 
+	}
+	public void visit(FactorBoolConst boolConst) {
+		int constValue = ((BoolConstant) boolConst.getBoolConst()).getB1();
+	   	Code.loadConst(constValue); 
+	}
+	
+	   
+	public void visit(PrintStatemtDetail print) {
+		Struct exprStruct = print.getExpr().struct;	
+		int currtype = Struct.None;
+    	//Array Check
+		if(exprStruct.getElemType() == null) {
+    		currtype = exprStruct.getKind();
+    	}else{
+    		currtype = exprStruct.getElemType().getKind();
+    	}	
+    	
+		
+		if(print.getPrintStmtOpt() instanceof PrintStatementOption) {
+			// Imamo zadat parametar sirine
+			int width = ((NumberConstant)((PrintStatementOption)print.getPrintStmtOpt()).getNumConst()).getN1();
+			Code.loadConst(width);
+			if(currtype == Struct.Int) {
+	    		Code.put(Code.print);
+	    	}else { //Bool, char
+	    		Code.put(Code.bprint);
+	    	}
+			
+		}else {
+			if(currtype == Struct.Int) {
+	    		Code.loadConst(5);
+	    		Code.put(Code.print);
+	    	}else { //Bool, char
+	    		Code.loadConst(1);
+	    		Code.put(Code.bprint);
+	    	}
+		}		
+		Code.put(Code.return_);
+	 }
 }
--- MJCompiler/src/rs/ac/bg/etf/pp1/ast/DesignatorActPars.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/ast/DesignatorActPars.java
@@ -1,6 +1,6 @@
 // generated with ast extension for cup
 // version 0.8
-// 27/5/2021 13:42:17
+// 27/5/2021 16:40:30
 
 
 package rs.ac.bg.etf.pp1.ast;
